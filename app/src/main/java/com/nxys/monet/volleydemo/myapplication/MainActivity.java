5beafd8a436775b71d5d01d77febb2ad7e9d9140
ningpeng67h36 <ningpeng67h36@gmail.com>
2016-03-08 15:49:06 +0800

    增加了一些功能

--- app/src/main/java/com/nxys/monet/volleydemo/myapplication/MainActivity.java
+++ app/src/main/java/com/nxys/monet/volleydemo/myapplication/MainActivity.java
@@ -1,13 +1,12 @@
 package com.nxys.monet.volleydemo.myapplication;
 
-import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
@@ -21,8 +20,9 @@ import com.android.volley.toolbox.Volley;
 public class MainActivity extends AppCompatActivity {
 
     private Button button = null;
+    private Button button2 = null;
     private TextView textView = null;
-    private Context context;
+//    private Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +37,7 @@ public class MainActivity extends AppCompatActivity {
                 RequestQueue mQueue = Volley.newRequestQueue(getApplicationContext());
 
                 //创建一个StringRequest对象,发出一条HTTP-get请求
-                StringRequest stringRequest = new StringRequest("http://www.baidu.com", //目标服务器的URL地址
+                StringRequest stringRequest = new StringRequest("http://www.github.com", //目标服务器的URL地址
                         new Response.Listener<String>() {
                             @Override
                             public void onResponse(String response) { //服务器响应成功的回调
@@ -56,4 +56,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void moveBall(View view) {
+//        String strTmp="点击Button03";
+//        Toast.makeText(MainActivity.this,"666",
+//                Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(MainActivity.this, BallActivity.class);
+        startActivity(intent);
+    }
+
+    public void calculator(View view) {
+
+        Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+        startActivity(intent);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+        startActivity(intent);
+    }
 }
